ef935a0e80347492e5d31438dae4633d37538f06
Daniel <dskoda@mit.edu>
2020-03-31 16:54:46 -0400

    created structure submodules

---
+++ moldocker/structure/complex.py
@@ -0,0 +1,27 @@
+from moldocker import utils
+from pymatgen.core import Molecule, Structure
+
+
+class Complex:
+    def __init__(
+        self,
+        host,
+        guest,
+    ):
+        """Constructor for host-guest pair.
+
+        Args:
+            host (Structure)
+            guest (Molecule)
+        """
+            
+        self.host = host
+        self.guest = guest
+
+    @property
+    def pose(self):
+        return utils.join_structures(self.guest, self.host)
+
+    def get_guest_host_distance(self):
+        """get distance between atoms from the guest and host"""
+        return utils.get_molecule_structure_distances(self.pose, len(self.host))
