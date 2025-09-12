e29022268a42b91a16e15981fdb5d3d98986c7a2
pafervi <pau-ferri@hotmail.com>
2025-09-12 16:16:28 -0400

    Clean up job.sh and improve instructions
    Removed unnecessary echo statements and clarified usage instructions.

--- examples/Cation_Anion/job.sh
+++ examples/Cation_Anion/job.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 # Runs until the first success is found  Also with Monte Carlo docking
-echo ""
-echo "In this example we must supply the --cation_indexes (start counting after the framework, remember 0 indexing)"
-echo "and the --acid_sites (oxygens bonded to the heteroatom)"
-echo ""
-echo "--acid_sites doesn't need all the acid sites present in the system, you can choose to sample for a preferred one if needed"
-echo ""
 echo "This example runs the docking of a DEB+ molecule into an Al-UTL zeolite framework"
 echo ""
 echo "If job fails to reach a final pose you can tune --threshold_catan, --threshold and --attempts parameters"
@@ -15,3 +9,4 @@ echo "Running Monte Carlo docking"
 echo ""
 python3 ../../scripts/dock.py structure.cif DEB+.xyz -d mcsuccess -s random -f min_catan_distance -o mcdocked --threshold_catan 3.5 --threshold 1.5 --attempts 200000 
 echo "Final pose save to mcdocked folder"
+
